5fca2ba62dd20103396fbfae1543d59d36998784
Chris Peters <chris@chrispeters.nl>
2016-07-18 13:31:05 +0200

    Updated to fit new hardware specs
    Modified and removed redundant code. Tested on new hardware.

---
+++ marvin_test/marvin_test/marvin_test.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // power RN2483
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  
+  // initialize both serial ports:
+  Serial.begin(57600);
+  Serial1.begin(57600);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+
+}
+
+void loop() {
+  
+  // read from port 1, send to port 0:
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte);
+  }
+
+  // read from port 0, send to port 1:
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte);
+  }
+  
+}
+
