96afc510c57ae861d4bc4b547829af20cbeddb64
Chris Peters <chris@chrispeters.nl>
2016-07-18 15:04:57 +0200

    Code cleanup

--- sleeperwillawaken/sleeperwillawaken.ino
+++ sleeperwillawaken/sleeperwillawaken.ino
@@ -1,47 +1,36 @@
 #include <avr/sleep.h>
 
-
 int wakePin = 2;                 // pin used for waking up
 int sleepStatus = 0;             // variable to store a request for sleep
 int count = 0;                   // counter
 
-void wakeUpNow()        // here the interrupt is handled after wakeup
+void wakeUpNow()
 {
-  // execute code here after wake-up before returning to the loop() function
-  // timers and code using timers (serial.print and more...) will not work here.
-  // we don't really need to execute any special functions here, since we
-  // just want the thing to wake up
+  /** 
+   *  execute code here after wake-up before returning to the loop() function
+   *  timers and code using timers (serial.print and more...) will not work here.
+   *  we don't really need to execute any special functions here, since we
+   *  just want the thing to wake up
+  **/
 }
 
 void setup()
 {
   pinMode(wakePin, INPUT);
-
-  Serial.begin(9600);
-
-  
-
-  attachInterrupt(0, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
-                                      // wakeUpNow when pin 2 gets LOW 
+  Serial.begin(57600);
+  attachInterrupt(0, wakeUpNow, LOW); /** use interrupt 0 (pin 2) and run function 
+                                      wakeUpNow when pin 2 gets LOW **/
 }
 
 void sleepNow()       
 {
-
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
-
     sleep_enable();         
-
     attachInterrupt(10,wakeUpNow, LOW); 
-
     sleep_mode();            
-    
-    sleep_disable();         // first thing after waking from sleep:
-                             // disable sleep...
-    detachInterrupt(0);      // disables interrupt 0 on pin 2 so the 
-                             // wakeUpNow code will not be executed 
-                             // during normal running time.
-
+    sleep_disable();         // first thing after waking from sleep is to disable sleep mode
+    detachInterrupt(0);      /** disables interrupt 0 on pin 2 so the 
+    wakeUpNow code will not be executed during normal running time. **/
 }
 
 void loop()
@@ -58,7 +47,6 @@ void loop()
     if (val == 'S') {
       Serial.println("Serial: Entering Sleep mode");
       delay(100);     // this delay is needed, the sleep 
-                      //function will provoke a Serial error otherwise!!
       count = 10;
       sleepNow();     // sleep function called here
     }
@@ -71,7 +59,6 @@ void loop()
   if (count >= 10) {
       Serial.println("Timer: Entering Sleep mode");
       delay(100);     // this delay is needed, the sleep 
-                      //function will provoke a Serial error otherwise!!
       count = 0;
       sleepNow();     // sleep function called here
   }
